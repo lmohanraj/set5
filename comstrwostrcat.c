909d507e1a32f74084e8d6793e533e2a5514d40e
lmohanraj <35128040+lmohanraj@users.noreply.github.com>
2018-02-05 11:15:21 +0000

    Create comstrwostrcat.c

---
+++ comstrwostrcat.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+  #include<string.h>
+  int main()
+  {
+  	char s1[100],s2[100];
+  	printf("enter first string\n");
+  	gets(s1);
+  	printf("enter second string\n");
+  	gets(s2);
+  	printf("%s%s",s1,s2);
+  	return 0;
+  }
